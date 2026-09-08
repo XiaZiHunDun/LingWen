b0d584569c8d034899e8a21d20ac3bbd6b81a5b7
XiaZiHunDun <cuiyangjs@163.com>
2026-09-08 19:51:16 +0800

    chore(infra): delete infra/project_config.py (Phase 38 P3-ARCHDEBT project_config cutover)

--- infra/project/__init__.py
+++ infra/project/__init__.py
@@ -1,5 +1,4 @@
 from lingwen_paths import *  # noqa: F403
 
 from infra.project_characters import *  # noqa: F403
-from lingwen_project_config import *  # noqa: F403
 from infra.project_init import *  # noqa: F403
